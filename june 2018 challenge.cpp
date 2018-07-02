c51d13f08668acbe1f555c0f9fbcc9365f4b269b
krisxlle <kriselle.t@gmail.com>
2018-07-01 23:11:02 -0700

    notes and syntax

--- june 2018 challenge.cpp
+++ june 2018 challenge.cpp
@@ -1,13 +1,12 @@
+//Girls Who Code Alumni Coding Challenge: June 2018
 
-//June 2018 Coding Challenge: Create a program that prints out the numbers 1 to 200 (inclusive).
+//Coding Challenge: Create a program that prints out the numbers 1 to 200 (inclusive).
 //If the number is even, print "Bam" instead of the number.
 //If the number is divisible by 3, print "Crash" instead.
 //If the number is divisible by three but not even, print "Boom" instead.
 
 void setup (){
     Serial.begin(9600);
-
-
 }
 
 void loop (){
